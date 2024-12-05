6d5352a7548a88e6beed7abe086d4e0e5409e9ac
Emma Fish <143717596+duckiechip167@users.noreply.github.com>
2024-12-05 05:31:36 +0000

    Index + Image changes

---
+++ java/script.js
@@ -0,0 +1,20 @@
+let text = document.getElementById('text');
+let hill1= document.getElementById('1');
+let hill2 = document.getElementById('2');
+let hill3 = document.getElementById('3');
+let hill4 = document.getElementById('4');
+let hill5 = document.getElementById('5');
+let plant = document.getElementById('plant');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    let maxScroll = window.innerHeight;
+
+    text.style.marginTop = Math.min(value * 2.5, maxScroll) + 'px';
+    hill5.style.left = Math.min(value * 1.5, maxScroll) + 'px';
+    hill4.style.left = Math.min(value * -1.5, maxScroll) + 'px';
+    hill1.style.top = Math.min(value * 1, maxScroll) + 'px';
+    hill2.style.top = Math.min(value * 0.5, maxScroll) + 'px';
+    hill3.style.top = Math.min(value * 0.25, maxScroll) + 'px';
+});
